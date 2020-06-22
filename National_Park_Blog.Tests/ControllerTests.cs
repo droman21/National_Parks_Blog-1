308472bd40819312a4307c59fdf12f277b94bdc8
Daniel Roman <dan.roman21@gmail.com>
2020-06-22 17:46:14 -0400

    Created models classes and properties

---
+++ National_Park_Blog.Tests/ControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace National_Park_Blog.Tests
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ National_Park_Blog/Models/National_Parks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace National_Park_Blog.Models
+{
+    public class National_Parks
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        //public virtual ICollection<Blog_Content> Blog Content {get; set;}
+
+        public National_Parks()
+        {
+
+        }
+        public National_Parks(string name, int id, string description, string image)
+        {
+            Name = name;
+            Id = id;
+            Description = description;
+            Image = image;
+        }
+
+    }
+}
