28728cc482d21feab488a24d09abff075108a01d
Dakota <Dakotawhittle1203@gmail.com>
2020-06-24 17:50:28 -0400

    filled out Irepository, NationalParkController, and created BlogContentCOntroller

---
+++ National_Park_Blog/Controllers/Blog_Content_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace National_Park_Blog.Controllers
+{
+    public class Blog_Content_Controller
+    {
+
+    }
+}
--- National_Park_Blog/Repositories/IRepository.cs
+++ National_Park_Blog/Repositories/IRepository.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace National_Park_Blog.Repositories
 {
-    public class IRepository
+    public interface IRepository<T> where T: class
     {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
     }
 }
