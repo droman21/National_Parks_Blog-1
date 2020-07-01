d0991de73b5a86e7b7ff9b23e73eb1c878f0613b
Daniel Roman <dan.roman21@gmail.com>
2020-07-01 17:20:40 -0400

    Added blogcontent controllertests

--- National_Park_Blog.Tests/BlogContentControllerTests.cs
+++ National_Park_Blog.Tests/BlogContentControllerTests.cs
@@ -26,6 +26,26 @@ namespace National_Park_Blog.Tests
             var result = underTest.Index();
             Assert.IsType<ViewResult>(result);
         }
+        [Fact]
+        public void Details_Returns_View()
+        {
+            var result = underTest.Details(2);
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void Details_Returns_Content_to_View()
+        {
+            var detailscontent = new Blog_Content();
+            blogRepo.GetById(3).Returns(detailscontent);
+            var result = underTest.Details(3);
+            Assert.Equal(detailscontent, result.Model);
+        }
+        [Fact]
+        public void CreateByNationalParkID_Returns_View()
+        {
+            var result = underTest.Create();
+            Assert.IsType<ViewResult>(result);
+        }
 
     }
 }
